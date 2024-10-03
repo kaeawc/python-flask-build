6f003ca4fa2958334baa68fbe261b99956f2a323
Jason Pearson <jason.d.pearson@gmail.com>
2024-10-03 01:06:43 -0500

    run tests and coverage, etc

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='python-flask-build',
+    version='0.0.1',
+    description='Python Flask Build',
+    url='https://github.com/kaeawc/python-flask/',
+    author='Jason Pearson',
+    author_email='jason.d.pearson@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "coverage==7.6.1",
+        "Delorean==1.0.0",
+        "ipdb==0.13.13",
+        "mock==5.1.0",
+        "nose2==0.15.1",
+        "pycodestyle==2.12.1",
+        "requests==2.32.3",
+        "tox==4.21.1",
+    ],
+    zip_safe=False)
